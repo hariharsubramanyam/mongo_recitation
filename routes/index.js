0e31c37738e67ea6d321c2ac0d98bbd18066e0e4
Harihar Subramanyam <hahasubbu@gmail.com>
2016-09-26 12:04:25 -0400

    Part 2

--- routes/index.js
+++ routes/index.js
@@ -4,33 +4,12 @@ var Activity = require('../models/activity');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Activity.find(function(err, activitiesList) {
-    if (err) console.log(err);
-    res.json({
-      'activities': activitiesList
-    });
-  });
+  // Implement this.
 });
 
 router.post('/', function(req, res, next) {
   activity = req.body;
-  Activity.create({ type: activity.type, 
-                    duration: activity.duration, 
-                    intensity: activity.intensity}, 
-    function(err, record) {
-      if (err) {
-        console.log(err);
-        res.json({
-          'success': false, 
-          'message': err.message
-        });
-      }
-      console.log(record.getDescription());
-      res.json({
-        'success': true, 
-        'activity': record
-      });
-  });
+  res.send("Need to implement this");
 });
 
 module.exports = router;
