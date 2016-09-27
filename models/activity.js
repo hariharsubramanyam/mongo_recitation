ae5dc5c4988d81b61d2670665ada69aa701812ef
Harihar Subramanyam <hahasubbu@gmail.com>
2016-09-27 19:10:08 -0400

    Starter

--- models/activity.js
+++ models/activity.js
@@ -2,6 +2,7 @@ var mongoose = require("mongoose");
 
 var activitySchema = mongoose.Schema({
   /* Exercise 1: Declare schema here */
+  type: String,
 });
 
 module.exports = mongoose.model("Activity", activitySchema);
